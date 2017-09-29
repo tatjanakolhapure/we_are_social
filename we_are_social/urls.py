2432c9487bfe3bb1f58aa38160e89bab4b1cb049
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-09-29 23:44:37 +0100

    come back to previous settings

--- we_are_social/urls.py
+++ we_are_social/urls.py
@@ -25,7 +25,6 @@ from django.views.static import serve
 from settings.base import MEDIA_ROOT
 from django.conf import settings
 from threads import views as forum_views
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -47,6 +46,7 @@ urlpatterns = [
 
     # Blog URLs
     url(r'^blog/', include('reusable_blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 
     # Forum URLs
     url(r'^forum/$', forum_views.forum),
@@ -61,5 +61,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns.append(url(r'^debug/', include(debug_toolbar.urls))),
-    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    urlpatterns.append(url(r'^debug/', include(debug_toolbar.urls)))
