d1be999f76f5e1624ce540c2cda66fbf9657945e
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-09-29 23:25:49 +0100

    move media url to dev settings and change media root to empty string in base settings

--- settings/base.py
+++ settings/base.py
@@ -110,5 +110,4 @@ TINYMCE_JS_ROOT = os.path.join(BASE_DIR, "static", "js",
 DISQUS_WEBSITE_SHORTNAME = 'mybootcampblog'
 
 # Storing blog images
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = ''
