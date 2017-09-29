19ee10799725162930d3aef1b446d0498ee29b4c
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-09-29 22:21:01 +0100

    connect to heroku database

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,11 @@
 from base import *
+import dj_database_url
 
 DEBUG = False
 
+# Load the ClearDB connection details from the environment variable
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 # Stripe environment variables
