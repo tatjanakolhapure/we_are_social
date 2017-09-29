f071e3192cce450b6c23abd15d4c9f922e5db1a3
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-09-29 22:59:12 +0100

    add static root in base settings

--- settings/base.py
+++ settings/base.py
@@ -95,7 +95,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+#static file route deployment to heroku - generates the folder when site uploaded.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"), # static directory at the project level
